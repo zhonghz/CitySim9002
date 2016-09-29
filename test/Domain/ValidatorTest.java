4e116fefb9438e0e6345868bdbc3b1efbfcfd3a5
zhonghz <huz25@pitt.edu>
2016-09-29 16:46:50 -0400

    InClass Exercise

--- test/Domain/ValidatorTest.java
+++ test/Domain/ValidatorTest.java
@@ -7,6 +7,9 @@ package Domain;
 
 import org.junit.Assert;
 import org.junit.Test;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  *
@@ -31,5 +34,15 @@ public class ValidatorTest {
     @Test
     public void ifArgumentIsNotAnIntegerReturnFalse() {
         Assert.assertFalse(new Validator().validateArgumentType(new String[] { "test" }));
+    }
+    
+    @Test
+    public void ifArgumentLengthAndArgumentTypeIsCorrect() {
+        Validator mockValidator = mock(Validator.class);
+        when(mockValidator.validateNumberOfArguments(any(String[].class))).thenReturn(true);
+        when(mockValidator.validateArgumentType(any(String[].class))).thenReturn(true);
+        when(mockValidator.validateArgumentType(any(String[].class))).thenCallRealMethod();
+        
+        Assert.assertTrue(mockValidator.validateArguments(new String[0]));
     }
 }
